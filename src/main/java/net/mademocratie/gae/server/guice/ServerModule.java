531a222bd789052448c70fbc9de7ae66f04161c5
Brice Vandeputte <brice.vandeputte@orange.com>
2013-03-25 09:50:16 +0100

    missing file

---
+++ src/main/java/net/mademocratie/gae/server/guice/ServerModule.java
@@ -0,0 +1,12 @@
+package net.mademocratie.gae.server.guice;
+
+/**
+ * ServerModule
+ * <p/>
+ * Last update  : $LastChangedDate$
+ * Last author  : $Author$
+ *
+ * @version : $Revision$
+ */
+public class ServerModule {
+}
