48d1164c691c3e277c9b037f93a6b620ed75408c
Brice Vandeputte <brice.vandeputte@orange.com>
2013-03-26 09:29:08 +0100

    0.2-rest : (draft ) refactor Contribution class design  [in progress]

--- src/main/java/net/mademocratie/gae/server/entities/Contribution.java
+++ src/main/java/net/mademocratie/gae/server/entities/Contribution.java
@@ -1,28 +1,11 @@
 package net.mademocratie.gae.server.entities;
 
-import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
 
-/**
- * IContribution
- * <p/>
- * Last update  : $LastChangedDate$
- * Last author  : $Author$
- *
- * @version : $Revision$
- */
-@XmlRootElement
 public abstract class Contribution {
-    Date date;
-    String contributionDetails = "oO";
-
     public Contribution() {};
 
-    public Date getDate() {
-        return date;
-    }
+    public abstract Date getDate();
 
-    public String getContributionDetails() {
-        return contributionDetails;
-    }
+    public abstract String getContributionDetails();
 }
