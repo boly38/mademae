12462ba9579f90c786f2f3d29d3dbd1ea7a3f52e
Brice Vandeputte <brice.vandeputte@orange.com>
2013-04-19 16:30:18 +0200

    0.3-rest : home page first acceptance test ^^ - using selenium - include gae:start as goal pre integration test

---
+++ src/test/java/net/mademocratie/gae/acceptancetest/HomePageIT.java
@@ -0,0 +1,29 @@
+package net.mademocratie.gae.acceptancetest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static com.thoughtworks.selenium.SeleneseTestBase.assertEquals;
+
+public class HomePageIT {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void home_page_should_be_displayed() {
+        this.driver.get("http://localhost:9000");
+        assertEquals("Ma Democratie", this.driver.getTitle());
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
